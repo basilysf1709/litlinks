fd34683e3872712a13002efbe3a1994efec64752
Basil Yusuf <basilyusuf1709@gmail.com>
2023-03-13 00:35:53 -0400

    Set the path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import { rootHandler, helloHandler } from './handlers';
+import path from "path";
+
+// Set the path for environment variable
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 const app = express();
 const port = process.env.PORT || '8000';
