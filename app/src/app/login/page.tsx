ac7be356b019f6f44387f66891601bd9617f03e2
Basil Yusuf <basilyusuf1709@gmail.com>
2023-08-15 10:21:01 +0530

    auth

---
+++ app/src/app/login/page.tsx
@@ -0,0 +1,56 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function Login() {
+  return (
+    <>
+    <Navbar />
+    <form action="/auth/login" method="post">
+      <label htmlFor="email">Email</label>
+      <input name="email" />
+      <label htmlFor="password">Password</label>
+      <input type="password" name="password" />
+      <button>Sign In</button>
+      <button formAction="/auth/sign-up">Sign Up</button>
+    </form>
+    <section className="">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a href="#" className="text-blue-700 flex items-center mb-6 text-2xl font-semibold text-gray-900">
+              Litlinks    
+          </a>
+          <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                      Sign in to your account
+                  </h1>
+                  <form className="space-y-4 md:space-y-6" action="/auth/login" method="post">
+                      <div>
+                          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                          <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" placeholder="name@company.com" required />
+                      </div>
+                      <div>
+                          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                          <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" required />
+                      </div>
+                      <div className="flex items-center justify-between">
+                          <div className="flex items-start">
+                              <div className="flex items-center h-5">
+                                <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
+                              </div>
+                              <div className="ml-3 text-sm">
+                                <label htmlFor="remember" className="text-gray-500">Remember me</label>
+                              </div>
+                          </div>
+                          <a href="#" className="text-sm font-medium text-blue-600 hover:underline">Forgot password?</a>
+                      </div>
+                      <button formAction="/auth/login" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                      <button formAction="/auth/sign-up" className="w-full text-white bg-blue-900 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign Up</button>
+                  </form>
+              </div>
+          </div>
+      </div>
+    </section>
+    <Footer />
+    </>
+  )
+}
