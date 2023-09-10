82a8dc39f2428c2b49950e49bf6dd01d2eb2b499
Basil Yusuf <basilyusuf1709@gmail.com>
2023-09-10 16:22:56 +0300

    Small change

--- app/src/components/AddCategory.tsx
+++ app/src/components/AddCategory.tsx
@@ -1,3 +1,4 @@
+'use client'
 declare global {
     interface Window {
       my_modal_1: HTMLDialogElement;
@@ -5,53 +6,12 @@ declare global {
   }
 
 export default function AddCategory() {
-    return (
-        <form className="m-8">   
-            <div className="block space-y-4 md:flex md:space-y-0 md:space-x-4">
-                <button onClick={()=>window.my_modal_1.showModal()} data-modal-target="medium-modal" data-modal-toggle="medium-modal" className="block w-full md:w-auto text-white bg-blue-900 hover:bg-blue-500 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button" >
-                    Add Category
-                </button>
-            </div>
-            <dialog id="my_modal_1">
-                <form>
-                    <div className="h-480 w-780">
-                        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
-                        <div className="relative">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                </svg>
-                            </div>
-                            <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-900 focus:border-blue-900" placeholder="Search Mockups, Logos..." required />
-                            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-900 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
-                        </div>
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                        <input type="password" id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
-                        <input type="password" id="repeat-password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-                    </div>
-                    <div className="flex items-start mb-6">
-                        <div className="flex items-center h-5">
-                        <input id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                        </div>
-                        <label htmlFor="terms" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
-                    </div>
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
-                    <div className="block space-y-4 md:flex md:space-y-0 md:space-x-4">
-                        <button onClick={()=>window.my_modal_1.close()} data-modal-target="medium-modal" data-modal-toggle="medium-modal" className="block w-full md:w-auto text-white bg-blue-900 hover:bg-blue-500 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button" >
-                            Add Category
-                        </button>
-                    </div>
-                </form>
-            </dialog>
-        </form>
+    return ( 
+        <div className="m-16 block space-y-4 md:flex md:space-y-0 md:space-x-4">
+            <button onClick={()=>window.my_modal_1.showModal()} data-modal-target="medium-modal" data-modal-toggle="medium-modal" className="block w-full md:w-auto text-white bg-blue-900 hover:bg-blue-500 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button" >
+                Add Category
+            </button>
+        </div>
+            
         )
 }
